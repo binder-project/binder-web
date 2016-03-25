ee12e30a72f9f4c8891bbb26e73ffaf55542efbb
freeman <the.freeman.lab@gmail.com>
2016-03-25 00:21:52 -0400

    use time in stream logs

--- server/binder.js
+++ server/binder.js
@@ -178,7 +178,7 @@ Binder.prototype.getLogs = function (templateName, startTime, cb) {
 Binder.prototype.streamBuildLogs = function (templateName, startTime) {
   var self = this
   var reader = getReader({ host: self.buildOpts.host })
-  var rawStream = reader.streamLogs({ app: templateName })
+  var rawStream = reader.streamLogs({ app: templateName, after: startTime })
   var msgStream = es.mapSync(function (data) {
     if (data) {
       return data.message
