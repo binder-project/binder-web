c0c98b6d35897d406e33bba8328f1e61e57fca0a
Andrew Osheroff <andrewosh@gmail.com>
2016-05-15 21:21:57 -0400

    hacky routing fix

--- site/router.js
+++ site/router.js
@@ -7,6 +7,10 @@ module.exports = sheetRouter('/', function (route) {
     route('/', function (params){ 
       actions.showOverview()(hxdx.dx)
     }),
+    route('/repo/:repo', function (params) {
+      var baseURL = 'http://' + window.location.hostname + ':' + window.location.port
+      window.location.href =  baseURL + '/repo/' + params.repo
+    }),
     route('/status/:project/:repo', function (params) {
       actions.showDetail(params.project + '-' + params.repo)(hxdx.dx)
     })
