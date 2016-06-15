47f667d5a3fcd1c90fd521266fbf36fd9e3c0ade
Andrew Osheroff <andrewosh@gmail.com>
2016-06-15 15:32:00 -0400

    added flag to remove search bar and overview list

--- site/components/overview/index.js
+++ site/components/overview/index.js
@@ -5,12 +5,30 @@ var build = require('./build.js')
 var search = require('./search.js')
 var list = require('./list.js')
 
+var conf = require('../../../conf/main.json')
+
+function showList (filter, model) {
+  if (conf.showOverview) {
+    return hx`${list(filter, model)}`
+  }
+  // TODO: something to display in the list's place?
+  return hx``
+}
+
+function showSearch () {
+  if (conf.showOverview) {
+    return hx`${search()}`
+  }
+  // TODO: something to display in the search bar's place?
+  return hx``
+}
+
 var overview = function (state) {
   return hx`
   <div>
     ${build()}
-    ${search()}
-    ${list(state.filter, state.model)}
+    ${showSearch()}
+    ${showList(state.filter, state.model)}
   </div>`
 }
 
