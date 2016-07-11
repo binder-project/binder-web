51ed6788f9a6fb1f9b919f764569c3be42f80abd
freeman <the.freeman.lab@gmail.com>
2016-07-11 15:44:48 -0400

    fix links

--- site/components/overview/welcome.js
+++ site/components/overview/welcome.js
@@ -22,7 +22,7 @@ module.exports = function () {
   <div style=${styles.container}>
     <h1 style=${styles.big}>Turn a GitHub repo into a collection of interactive notebooks</h1>
     <h2 style=${styles.medium}>Have a repository full of Jupyter notebooks? With Binder, you can add a badge that opens those notebooks in an executable environment, making your code immediately reproducible by anyone, anywhere.</h2>
-    <h2 style=${styles.medium}>100% free and <a className='welcome-link' href='https://github.com/binder-project'>open source</a>. Browse <a className='welcome-link' href='/feed'>examples</a>. Read the <a className='welcome-link' href='/faq'>FAQ</a>.</h2>
+    <h2 style=${styles.medium}>100% free and <a className='welcome-link' href='https://github.com/binder-project'>open source</a>. Browse <a className='welcome-link' href='https://github.com/binder-project?query=example'>examples</a>. Read the <a className='welcome-link' href='http://docs.mybinder.org/faq'>FAQ</a>.</h2>
   </div>
   `
 }
