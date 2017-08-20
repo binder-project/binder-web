f6a965dec2724e13d6205f0db3c13b97ef91e896
Chris Holdgraf <choldgraf@gmail.com>
2017-08-20 08:36:00 -0700

    Update welcome.js

--- site/components/overview/welcome.js
+++ site/components/overview/welcome.js
@@ -27,7 +27,7 @@ module.exports = function () {
   return hx`
   <div style=${styles.container}>
     <h1 style=${styles.big}>Turn a GitHub repo into a collection of interactive notebooks</h1>
-    <h2 style=${styles.attention}>Binder is upgrading and will be better than ever soon.<br />Check out <a href="http://beta.mybinder.org">beta.mybinder.org</a> to try it out!</h2>
+    <h2 style=${styles.attention}>Binder is upgrading and will be better than ever soon.<br />Try it at <a href="http://beta.mybinder.org">beta.mybinder.org</a>!</h2>
     <h2 style=${styles.medium}>Have a repository full of Jupyter notebooks? With Binder, you can add a badge that opens those notebooks in an executable environment, making your code immediately reproducible by anyone, anywhere.</h2>
     <h2 style=${styles.medium}>100% free and <a className='welcome-link' href='https://github.com/binder-project'>open source</a>. Browse <a className='welcome-link' href='https://github.com/binder-project?q=example'>examples</a>. Read the <a className='welcome-link' href='http://docs.mybinder.org/faq'>FAQ</a>.</h2>
   </div>
